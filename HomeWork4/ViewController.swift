1d33d5b915b4032e60e2b02a9544084b12d1bf68
AseinovSyimyk <aseinovsyimyk52@gmail.com>
2023-10-30 21:41:21 +0600

    Initial Commit

---
+++ HomeWork4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork4
+//
+//  Created by User on 30/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
